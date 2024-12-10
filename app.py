106b24aa61a1bfa2d25a0480da24053958957dd0
Geargrind <lass347s@stud.kea.dk>
2024-12-10 10:56:59 +0100

    Added a small bit of code at the end of app.py

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def predict():
         return render_template('index.html', result='error ' + str(e))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
